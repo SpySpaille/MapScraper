0fe18508b8b0da509561ab67954c2cfa7b9ac1e3
SpySpaille <45824217+SPY001720@users.noreply.github.com>
2023-02-26 16:17:44 +0100

    Add sound gette

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ const outputDir = './output';
 const gpath = "E:\\Program Files (x86)\\Steam\\steamapps\\common\\GarrysMod\\garrysmod";
 const file = "rp_site_kb_v1.vmf";
 const vmfContent = fs.readFileSync(file, 'utf-8');
-const TextureGettersURL = "https://raw.githubusercontent.com/SpySpaille/MapScraper/main/TextureGetter.txt";
+const TextureGettersURL = "https://raw.githubusercontent.com/SpySpaille/MapScraper/main/Getters/TextureGetter.txt";
 const modelsvariants = [".mdl", ".vvd", ".phy", ".dx90.vtx", ".dx80.vtx", ".sw.vtx", ".ani"];
+const SoundGettersURL = "https://raw.githubusercontent.com/SpySpaille/MapScraper/main/Getters/SoundGetter.txt";
 
 fse.removeSync(outputDir);
 fse.ensureDirSync(outputDir);
 
-fetch(TextureGettersURL).then(response => response.text()).then(text => {
 
-    // Get VTFs from materials
-    function VTFfromVMT(sourcePath, outputDir) {
+// Get VTFs from materials
+function VTFfromVMT(sourcePath, outputDir) {
+    fetch(TextureGettersURL).then(response => response.text()).then(text => {
         const TextureGetters = text.split('\n').map(line => line.trim());
         const textures = [];
         let materialContent;
@@ -44,80 +45,100 @@ fetch(TextureGettersURL).then(response => response.text()).then(text => {
                 fse.copySync(sourcePath, destPath);
             } catch (err) { return; }
         });
+    });
+}
+
+// Get all materials
+function getMaterials() {
+    const regex = /"material"\s*"([^"]+)"/g;
+    const materials = [];
+
+    let match;
+    while (match = regex.exec(vmfContent)) {
+        const material = match[1];
+        if (!materials.includes(material)) {
+            materials.push(material);
+        }
     }
 
-    // Get all materials
-    function getMaterials() {
-        const regex = /"material"\s*"([^"]+)"/g;
-        const materials = [];
+    materials.forEach(material => {
+        const sourcePath = path.join(gpath, 'materials', material + '.vmt');
+        const destPath = path.join(outputDir, 'materials', material.toLowerCase() + '.vmt');
+        try {
+            fse.copySync(sourcePath, destPath);
+        } catch (err) { return; }
 
-        let match;
-        while (match = regex.exec(vmfContent)) {
-            const material = match[1];
-            if (!materials.includes(material)) {
-                materials.push(material);
+        VTFfromVMT(sourcePath, outputDir);
+    });
+}
+
+// Get all models
+function getModels() {
+    const regex = /"model"\s*"([^"]+)"/g;
+    const models = [];
+
+    let match;
+    while (match = regex.exec(vmfContent)) {
+        const model = match[1];
+        if (!models.includes(model)) {
+            if (model.endsWith('.mdl')) {
+                models.push(model);
             }
         }
+    }
 
-        materials.forEach(material => {
-            const sourcePath = path.join(gpath, 'materials', material + '.vmt');
-            const destPath = path.join(outputDir, 'materials', material.toLowerCase() + '.vmt');
+    models.forEach(async model => {
+        modelsvariants.forEach(variant => {
+            const sourcePath = path.join(gpath, model.replace('.mdl', variant));
+            const destPath = path.join(outputDir, model.replace('.mdl', variant));
             try {
                 fse.copySync(sourcePath, destPath);
             } catch (err) { return; }
-
-            VTFfromVMT(sourcePath, outputDir);
         });
-    }
 
-    // Get all models
-    function getModels() {
-        const regex = /"model"\s*"([^"]+)"/g;
-        const models = [];
+        try {
+            let mdlData = fs.readFileSync(path.join(gpath, model));
+            let mdl = new MDL();
+            mdl.import({ mdlData });
+            // Put mdl.getMetadata().textureDirs to lowercase into an array 
+            const textures = mdl.getMetadata().textures.map(texture => texture.toLowerCase());
+            const textureDirs = mdl.getMetadata().textureDirs.map(textureDir => textureDir.toLowerCase());
+            textureDirs.forEach(textureDir => {
+                textures.forEach(texture => {
+                    const sourcePath = path.join(gpath, 'materials', textureDir, texture + '.vmt');
+                    const destPath = path.join(outputDir, 'materials', textureDir, texture + '.vmt');
+                    console.log(sourcePath);
+                    console.log(destPath);
+                    try {
+                        fse.copySync(sourcePath, destPath);
+                    } catch (err) { return; }
+                    // Get VTFs from materials
+                    VTFfromVMT(sourcePath, outputDir);
+                });
+            });
+        } catch (err) { return; }
+    });
+}
 
-        let match;
-        while (match = regex.exec(vmfContent)) {
-            const model = match[1];
-            if (!models.includes(model)) {
-                if (model.endsWith('.mdl')) {
-                    models.push(model);
+// Get all sounds
+function getSounds() {
+    fetch(SoundGettersURL).then(response => response.text()).then(text => {
+        const SoundGetters = text.split('\n').map(line => line.trim());
+        const sounds = [];
+
+        SoundGetters.forEach(getter => {
+            const regex = new RegExp(`\\${getter} \\s*([^\\s]+)`, 'g');
+            let match;
+            while (match = regex.exec(vmfContent)) {
+                const sound = match[1];
+                if (!sounds.includes(sound)) {
+                    console.log(sound);
+                    sounds.push(sound);
                 }
             }
-        }
-
-        models.forEach(async model => {
-            modelsvariants.forEach(variant => {
-                const sourcePath = path.join(gpath, model.replace('.mdl', variant));
-                const destPath = path.join(outputDir, model.replace('.mdl', variant));
-                try {
-                    fse.copySync(sourcePath, destPath);
-                } catch (err) { return; }
-            });
-
-            try {
-                let mdlData = fs.readFileSync(path.join(gpath, model));
-                let mdl = new MDL();
-                mdl.import({ mdlData });
-                // Put mdl.getMetadata().textureDirs to lowercase into an array 
-                const textures = mdl.getMetadata().textures.map(texture => texture.toLowerCase());
-                const textureDirs = mdl.getMetadata().textureDirs.map(textureDir => textureDir.toLowerCase());
-                textureDirs.forEach(textureDir => {
-                    textures.forEach(texture => {
-                        const sourcePath = path.join(gpath, 'materials', textureDir, texture + '.vmt');
-                        const destPath = path.join(outputDir, 'materials', textureDir, texture + '.vmt');
-                        console.log(sourcePath);
-                        console.log(destPath);
-                        try {
-                            fse.copySync(sourcePath, destPath);
-                        } catch (err) { return; }
-                        // Get VTFs from materials
-                        VTFfromVMT(sourcePath, outputDir);
-                    });
-                });
-            } catch (err) { return; }
         });
-    }
+    });
+}
 
-    getMaterials();
-    getModels();
-});
+getMaterials();
+getModels();
